7db16ab59a434c99a95d4569efe9a3acb951a46d
vishnujsuresh <mannathichira@gmail.com>
2021-03-26 23:00:08 +0530

    logandsignup

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button= findViewById<Button>(R.id.signupButton)
+
+
+        button.setOnClickListener{
+            val intent = Intent(this@MainActivity,MainActivity2signup::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
     }
 }
