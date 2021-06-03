3594a82be6c479fd82200359cc93853cabce9c6c
vishnujsuresh <vishnujsuresh5055@gmail.com>
2021-06-04 02:19:09 +0530

    doc submit ok

--- app/src/main/java/com/example/myapplication/DocAdd.kt
+++ app/src/main/java/com/example/myapplication/DocAdd.kt
@@ -1,11 +1,59 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class DocAdd : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doc_add)
+
+        val mAuth = FirebaseAuth.getInstance()
+        var database = FirebaseDatabase.getInstance().getReference("doctor")
+        val submit = findViewById<Button>(R.id.dr_add)
+        val username = mAuth.currentUser
+        val uid = username!!.uid
+
+
+        submit.setOnClickListener {
+
+            var dssn = findViewById<EditText>(R.id.dr_assn)
+            val dname = findViewById<EditText>(R.id.dr_aname)
+            val dclinic = findViewById<EditText>(R.id.dr_aclinic)
+            val dmob = findViewById<EditText>(R.id.dr_aphn)
+            val dexp = findViewById<EditText>(R.id.dr_aexp)
+            val dspec = findViewById<EditText>(R.id.dr_aspec)
+
+            var ssn = dssn.text.toString().toInt()
+            var name = dname.text.toString()
+            var clinic = dclinic.text.toString()
+            var mob = dmob.text.toString()
+            var exp = dexp.text.toString().toInt()
+            var spec = dspec.text.toString()
+
+            if (mob.length > 0 && ssn>0 && name.length>0 && clinic.length>0 && exp>0 && spec.length>0) {
+                if (mob.length == 10) {
+                    database.child(uid.toString()).setValue(doctor(uid, ssn, name, clinic, mob, exp, spec))
+                    Toast.makeText(this, "profile Updated ", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, DocHome::class.java))
+                }
+                else{
+                    Toast.makeText(this, "Check Your Mobile  Number", Toast.LENGTH_LONG).show()
+                }
+            }
+            else{
+                Toast.makeText(this, "All fields must be filled", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/doctor.kt
+++ app/src/main/java/com/example/myapplication/doctor.kt
@@ -1,20 +1,20 @@
 package com.example.myapplication
 
 class doctor {
-    var dr_ssn=0
-    var dr_name=""
-    var dr_addr=""
-    var dr_mobno=""
-    var dr_exp=0
-    var dr_spec=""
-    var dr_uid=""
-    constructor(uid:String,dr_ssn:Int,dr_name:String,dr_addr:String,dr_mobno:String,dr_exp:Int,dr_spec:String){
-        this.dr_ssn=dr_ssn
-        this.dr_uid=uid
-        this.dr_name=dr_name
-        this.dr_addr=dr_addr
-        this.dr_mobno= dr_mobno
-        this.dr_exp=dr_exp
-        this.dr_spec=dr_spec
+    var ssn=0
+    var name=""
+    var clinic=""
+    var mobno=""
+    var exp=0
+    var spec=""
+    var uid=""
+    constructor(uid:String,ssn:Int,name:String,clinic:String,mobno:String,exp:Int,spec:String){
+        this.ssn=ssn
+        this.uid=uid
+        this.name=name
+        this.clinic=clinic
+        this.mobno= mobno
+        this.exp=exp
+        this.spec=spec
     }
 }
