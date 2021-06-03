18267b953523e2effb7d3ba730be3e118d761a7a
vishnujsuresh <vishnujsuresh5055@gmail.com>
2021-06-03 23:15:24 +0530

    messed up fix

--- app/src/main/java/com/example/myapplication/welcome.kt
+++ app/src/main/java/com/example/myapplication/welcome.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val dsignbtn=findViewById<Button>(R.id.dr_signupButton);
+        dsignbtn.setOnClickListener(View.OnClickListener{
+                View->signupfun()
+        } )
+
+
+    }
+    private fun signupfun()
+    {
+        startActivity(Intent(this,dr_signup::class.java))
     }
 }
