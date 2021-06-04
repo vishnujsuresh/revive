cd8ee666942d93b249ee40e5a7fdd78ed33a74bc
vishnujsuresh <vishnujsuresh5055@gmail.com>
2021-06-04 22:32:47 +0530

    dr login forgot

--- app/src/main/java/com/example/myapplication/DocForgot.kt
+++ app/src/main/java/com/example/myapplication/DocForgot.kt
@@ -1,11 +1,55 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class DocForgot : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doc_forgot)
+
+
+
+
+        val forgotpass = findViewById<Button>(R.id.dr_reset_pass)
+
+
+        forgotpass.setOnClickListener(View.OnClickListener{
+                View->reset()
+        })
+
+
+    }
+
+    private fun reset(){
+
+
+        val eid = findViewById<View>(R.id.dr_formail) as EditText
+        var reset_id = eid.text.toString()
+        if (reset_id.isNotEmpty()) {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(reset_id).addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+
+                    Toast.makeText(this, "Password reset link sent to your emial id !!! ", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this,welcome::class.java))
+                    finish()
+                }
+                else{
+                    Toast.makeText(this, task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        else{
+            Toast.makeText(this, "Enter your emial id !!! ", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/welcome.kt
+++ app/src/main/java/com/example/myapplication/welcome.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -31,9 +33,46 @@ class welcome : AppCompatActivity() {
             startActivity(Intent(this,DocForgot::class.java))
         }
 
+        val dlog = findViewById<Button>(R.id.dr_login_button)
+        dlog.setOnClickListener (View.OnClickListener{
+            View -> dr_login()
+        })
+
     }
     private fun signupfun()
     {
         startActivity(Intent(this,docSignup::class.java))
+    }
+
+    private fun dr_login(){
+
+
+
+        val dr_emailTxt = findViewById<View>(R.id.dr_loginmail) as EditText
+        val dr_passwordTxt = findViewById<View>(R.id.dr_password) as EditText
+        var email = dr_emailTxt.text.toString()
+        var password = dr_passwordTxt.text.toString()
+        if(!email.isEmpty() && !password.isEmpty()){
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    startActivity(Intent(this,DocHome::class.java))
+                    Toast.makeText(this,"Login Successful !! ",Toast.LENGTH_LONG).show()
+                    finish()
+                }else
+                {
+                    Toast.makeText(this, task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            })
+
+
+        }
+        else
+        {
+            Toast.makeText(this,"Please provide the login details !!! ",Toast.LENGTH_LONG).show()
+        }
+
+
+
     }
 }
