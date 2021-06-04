a9d24dc8865b5b368f5f9678a176c2041380d0a4
vishnujsuresh <vishnujsuresh5055@gmail.com>
2021-06-05 00:51:08 +0530

    ok

--- app/src/main/java/com/example/myapplication/DocHome.kt
+++ app/src/main/java/com/example/myapplication/DocHome.kt
@@ -1,11 +1,36 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class DocHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doc_home)
+
+        var database = FirebaseDatabase.getInstance().getReference("user")
+        val mAuth = FirebaseAuth.getInstance()
+
+
+        val dr_signout =findViewById<ImageButton>(R.id.dr_home_signout)
+
+
+        val username = mAuth.currentUser
+        val uid = username!!.uid
+
+        dr_signout.setOnClickListener{
+
+            mAuth.signOut()
+            Toast.makeText(this, "signing out  ", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this,welcome::class.java))
+            finish()
+        }
+
     }
 }
